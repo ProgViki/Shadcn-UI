c4a58910a1ca39e8237170b99e79f08e03e08705
Victor Ogunwehin <victorogunwehin@gmail.com>
2025-07-18 18:03:42 +0100

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
-import { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Layout } from "./components/layout"
+import { HomePage } from "./pages/home"
+import { DocsPage } from "./pages/docs"
+import { ComponentsPage } from "./pages/components"
 
-import "./App.css";
-import { ConfigProvider } from "antd";
-import { BrowserRouter } from "react-router-dom";
-import { ErrorBoundary } from "react-error-boundary";
-import AppRoutes from "./routes/AppRoutes";
-import { PopupProvider } from "./context/PopupContext";
-import PageError from "./component/PageError";
-import { Loading } from "./component/global/Loading";
-
-function App() {
+export function App() {
   return (
-    <ErrorBoundary fallback={<PageError />}>
-      <Suspense fallback={<Loading />}>
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: "#0A96CC",
-            },
-            components: {
-              Form: {
-                itemMarginBottom: 6,
-                verticalLabelPadding: 1,
-                labelFontSize: 13,
-              },
-
-              Input: {
-                activeBg: "inherit",
-              },
-              Tabs: {
-                itemSelectedColor: "green",
-                itemHoverColor: "green",
-                colorPrimary: "green",
-              },
-            },
-          }}
-        >
-          <PopupProvider>
-            <BrowserRouter>
-              <AppRoutes />
-            </BrowserRouter>
-          </PopupProvider>
-        </ConfigProvider>
-      </Suspense>
-    </ErrorBoundary>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="docs" element={<DocsPage />} />
+          <Route path="components" element={<ComponentsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
--- src/component/layout.tsx
+++ src/component/layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom"
+
 import { Header } from "./header"
 import { Footer } from "./footer"
 
@@ -7,7 +7,7 @@ export function Layout() {
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
-        <Outlet />
+        {/* <Outlet /> */}
       </main>
       <Footer />
     </div>
